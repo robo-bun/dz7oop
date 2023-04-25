a9a981b6b85802223a1511e36187f1b4aef5dd07
Tanya <122279254+robo-bun@users.noreply.github.com>
2023-04-25 22:13:24 +0400

    Update index.php

--- index.php
+++ index.php
@@ -71,3 +71,67 @@ echo $student->getCourse() . "\n";
 2.2 Методы для получения значений полей (геттеры)
 2.3 Методы для установки значений полей (сеттеры)
 2.4 Методы для вывода информации об автомобиле
+
+
+class Car {
+
+    private $brand;
+    private $model;
+    private $color;
+    private $year;
+
+
+    public function __construct($brand, $model, $color, $year) {
+        $this->brand = $brand;
+        $this->model = $model;
+        $this->color = $color;
+        $this->year = $year;
+    }
+
+    public function getBrand() {
+        return $this->brand;
+    }
+
+    public function getModel() {
+        return $this->model;
+    }
+
+    public function getColor() {
+        return $this->color;
+    }
+
+    public function getYear() {
+        return $this->year;
+    }
+
+    public function setBrand($brand) {
+        $this->brand = $brand;
+    }
+
+    public function setModel($model) {
+        $this->model = $model;
+    }
+
+    public function setColor($color) {
+        $this->color = $color;
+    }
+
+    public function setYear($year) {
+        $this->year = $year;
+    }
+}
+
+$car1 = new Car('BMW', 'M8', 'black', '2022');
+
+//echo $car1->getBrand() . "\n";
+//echo $car1->getModel() . "\n";
+//echo $car1->getColor() . "\n";
+//echo $car1->getYear() . "\n";
+
+
+
+
+// 3. Создать класс "Круг", у которого есть поле "радиус".
+
+3.1 Конструктор класса, который принимает значение радиуса и устанавливает его соответствующим образом.
+3.4 Метод для вычисления площади круга - calculateArea()
